1f3262565a67f0338d88f9b148f6242468cdab4f
cee breezy <djmisscbrown@hotmail.com>
2022-05-04 15:03:02 -0400

    un commented code

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@
 const router = require('express').Router();
 
 // Import all of the API routes 
-// const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 
 // add prefix of `/api` to all of the api routes
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
 // 404 Status error message
 router.use((req, res) => {
