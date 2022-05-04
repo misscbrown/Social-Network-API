04e2cbdc52e5e8cceb853168f536e86c89c547fd
cee breezy <djmisscbrown@hotmail.com>
2022-05-04 14:44:31 -0400

    created new file

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
+// Require express router
 const router = require('express').Router();
-// const userRoutes = require('./user-routes.js');
-// const thoughtRoutes = require('./thought-routes');
 
-// router.use('/api/users', userRoutes);
-// router.use('/api/thoughts', thoughtRoutes);
+// Import all of the API routes 
+// const apiRoutes = require('./api');
 
+// add prefix of `/api` to all of the api routes
+// router.use('/api', apiRoutes);
+
+// 404 Status error message
 router.use((req, res) => {
-    res.status(404).send('<h1>🌍 404 Error!</h1>'); 
-});
+    res.status(404).send('<h1>🚫404 Error....</h1>');
+  });
 
+// Module exports router
 module.exports = router;
