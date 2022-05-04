e0d54481e6c6e7bd28fe851df0fd14678d27f286
cee breezy <djmisscbrown@hotmail.com>
2022-05-04 15:02:51 -0400

    set up controllers get all

---
+++ controllers/user-controller.js
@@ -0,0 +1,30 @@
+// Require Users Model
+const {Users} = require('../models');
+
+// Set up Users Controller
+const usersController = {
+    
+    // Create a new User
+    createUsers({body}, res) {
+        Users.create(body)
+        .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // Get All Users
+    getAllUsers(req, res) {
+        Users.find({})
+        // populate users thoughts
+        .populate({path: 'thoughts', select: '-__v'})
+        // populate user friends
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        // .sort({_id: -1})
+        .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    
