9c99682ff7b36568604e3495f5dc5be8b66a70e6
cee breezy <djmisscbrown@hotmail.com>
2022-05-04 14:45:09 -0400

    changed file location

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+    createThought,
+    addReaction,
+    removeReaction,
+    removeThought,
+    getAllThoughts
+} = require('../controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+    router  
+    .route('/:thoughtId')
+    .post(addReaction)
+    .delete(removeThought);
+    router
+    .route('/:thoughtId/:reactionId')
+    .delete(removeReaction);
+    
+
+    module.exports = router;
+    
